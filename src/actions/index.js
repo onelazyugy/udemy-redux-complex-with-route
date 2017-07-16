ee42ad55f76f88523d107ba72e44e1e6a3b6264a
viet <viettangle12@gmail.com>
2017-07-16 12:27:46 -0400

    add action creator to fetch list of posts

---
+++ src/actions/index.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+export const FETCH_POSTS = 'fetch_posts';
+
+const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
+const API_KEY = '?key=1050';
+
+export function fetchPosts() {
+    const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);//request is a promise not the actual data, redux promise will resolve into the data response
+    return {    
+        type: 'FETCH_POSTS',
+        payload: request
+    };
+}
