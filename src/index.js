dfb3a90a7301e0aeb18dad7e431cdb37de34c74d
viet <viettangle12@gmail.com>
2017-07-16 20:42:55 -0400

    add switch from react router

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter, Route } from 'react-router-dom'; //BR interact with History package
+//Switch takes in collection of routes, only render the first route that matches current url
+//BrowserRouter interact with History package
+import { BrowserRouter, Route, Switch } from 'react-router-dom'; 
 import promise from 'redux-promise';
 
 import reducers from './reducers';
 import PostsIndex from './components/posts_index';
+import PostsNew from './components/posts_new';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
@@ -14,7 +17,10 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        <Route path="/" component={PostsIndex} />
+        <Switch>
+          <Route path="/posts/new" component={PostsNew} /> {/* most specific need to be at the top */}
+          <Route path="/" component={PostsIndex} />
+        </Switch>
       </div>
     </BrowserRouter>
   </Provider>
