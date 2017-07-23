76e402cf02e3e9e59deed13ba1e24d66072d0a1b
viet <viettangle12@gmail.com>
2017-07-22 22:49:03 -0400

    wire up the action for submit new post

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,23 +1,31 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions';
 
 class PostsNew extends Component {
     
     renderField(field) {
+        const { meta: {touched, error} } = field; //destructure..field take out meta, meta take out touched and error
+        const className = `form-group ${touched && error ? 'has-danger' : ''}`;
         return (
-            <div className="form-group"> 
+            <div className={className}> 
                 <label>{field.label}</label>
                 <input 
                     className="form-control"
                     type="text"
                     {...field.input}
                 />
-                {field.meta.touched ? field.meta.error : ''}
+                <div className="text-help">
+                    {touched ? error : ''}
+                </div>
             </div>
         );
     }
     
     onSubmit(values) {
+        this.props.createPost(values);
         console.log(values);
     }
 
@@ -42,6 +50,7 @@ class PostsNew extends Component {
                     component={this.renderField}
                 />
                 <button type="submit" className="btn btn-primary">Submit</button>
+                <Link to="/" className="btn btn-danger">Cancel</Link>
             </form>
         );
     }
@@ -70,4 +79,6 @@ function validate(values) {
 export default reduxForm({
     validate,
     form: 'PostsNewForm'//this is one of the form, this reduxForm helper that allows redux form to communicate directly from component to th reducer that we defined
-})(PostsNew);
+})(
+    connect(null, { createPost }) (PostsNew)
+);
