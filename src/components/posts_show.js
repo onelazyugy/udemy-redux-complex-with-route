d5478a8c869295066ef79979e5f100c1cd75aa5f
viet <viettangle12@gmail.com>
2017-07-23 10:51:47 -0400

    add ability to view a single post

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions';
 
 class PostsShow extends Component {
+    componentDidMount() {
+        const { id } = this.props.match.params; //from react-router
+        this.props.fetchPost(id);
+    }
+
     render() {
+        const { post } = this.props;//refer to as destructuring, take the post variable out from the props, now you can use post constant
+        if(!post) {
+            return <div> Loading...</div>;
+        }
+        
         return (
             <div>
-                Posts Show!
+                <h3>{post.title}</h3>
+                <h6>Categories: {post.categories}</h6>
+                <p>{post.content}</p>
             </div>
         );
-    };
+    }
 }
 
-export default PostsShow;
+function mapStateToProps({ posts }, ownProps) { //ownProps is props available to this component when this component get created
+    //posts is a big list of all posts
+    //we only want a single post
+    return { post: posts[ownProps.match.params.id] };
+}
+
+export default connect(mapStateToProps, {fetchPost})(PostsShow);
--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,9 +1,20 @@
 import _ from 'lodash';
-import {FETCH_POSTS}   from '../actions';
+import {FETCH_POSTS, FETCH_POST}   from '../actions';
 
 
 export default function(state = {}, action) {
     switch (action.type) {
+        case FETCH_POST: 
+            //ES5 syntax
+            /*
+            const post = action.payload.data;
+            const newState = { ...state }; //take current posts and return new post object
+            newState[post.id] = post;
+            return newState;
+            */
+
+            //ES6 syntax
+            return { ...state, [action.payload.data.id]: action.payload.data };
         case FETCH_POSTS:
             return _.mapKeys(action.payload.data, 'id');
         default:
